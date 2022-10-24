728cf106c56a955454e7da9ee6785eceb6736036
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-24 16:35:45 +0000

    Task 0 takes a point to an int and updates it to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - take a pointer to an int and updates to 98
+ * @n : a pointer
+ * Return: 0 Always
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+The tasks here deals with Pointers, Arrays and Strings
+Task 0 takes a pointer to an int and updates to 98
