69e6ef38cb116766c74119d04aaeef90eb68a161
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-25 15:38:57 +0000

    prints n elements of an array of integers, followed by a new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,24 +8,24 @@
  */
 int main(void)
 {
-	int array[9];
+	int arr[9];
 
-	int i;
+	int *a;
 
 	printf("\n\Read and Print elements of an array:\n");
 	printf("---------------------------------------\n");
 
 	printf("0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 :\n");
-	for (i = 0; i < 9; i++)
+	for (*a = 0; *a < 9; *a++)
 	{
-		printf("element - %d : ", i);
-		scanf("%d", &arr[i]);
+		printf("element - %d : ", *a);
+		scanf("%d", &arr[*a]);
 	}
 
 	printf("\nElements in array are: ");
-	for (1 = 0; i < 9; i++)
+	for (*a = 0; i < 9; *a++)
 	{
-		printf("%d ", arr[i]);
+		printf("%d ", arr[*a]);
 	}
 	printf("\n");
 }
