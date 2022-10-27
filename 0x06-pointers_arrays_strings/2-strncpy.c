6125569bc8a3ee8bed0a1e7b2496325553ca154d
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-27 22:36:32 +0000

    Hot fix Task two function copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,8 +5,10 @@
  * @dest: the buffer storing the string
  * @src: the source string
  * @n: the maximum number of bytes to be copied from src
+ *
  * Return: Pointer to the string dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
