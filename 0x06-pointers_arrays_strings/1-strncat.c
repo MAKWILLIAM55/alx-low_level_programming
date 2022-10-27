148339e927325e533c9705fa0bf219c530210c51
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-27 21:58:51 +0000

    hot fix task one concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,27 +5,17 @@
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
- * Return: dest
+ * Return: a pointer to the dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int j;
+	int index = 0, dest_len = 0;
 
-	int K;
+	while (dest[index++])
+		dest_len++;
 
-	j = 0;
-	while (dest[j] != '\0')
-	{
-		j++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
-	}
-	k = 0;
-	while (k < n && src[k] != '\0')
-	{
-		dest[j] = src[k];
-		j++;
-		k++;
-	}
-	dest[j] = '\0';
 	return (dest);
 }
