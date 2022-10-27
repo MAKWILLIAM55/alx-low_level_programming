713a430c6b0c2690e00ec3515f4663aef6b9e883
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-27 13:10:24 +0000

    Task 7 prints the lowercase alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char u;
+
+	u = 'z';
+	while
+		(u >= 'a')
+		{
+			putchar(u);
+			u--;
+		}
+	putchar('\n');
+	return (0);
+}
