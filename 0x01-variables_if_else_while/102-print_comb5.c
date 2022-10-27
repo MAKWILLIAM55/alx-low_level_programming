816ef2f90652e8d61f041df7af52b5d009dfa565
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-27 15:25:46 +0000

    Task 102 Prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return a combination of numbers
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+	int j, k;
+
+	for (j = 0; j < 100; j++)
+	{
+		for (k = 0; k < 100; k++)
+		{
+			if (j < k)
+			{
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				putchar(' ');
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				if (j != 98 || k != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
