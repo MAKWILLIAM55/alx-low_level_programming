fae9a402f63eed9566ecdce4cf811d64ebac2e01
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-28 11:53:27 +0000

    Task 7 encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,11 +9,12 @@
 char *leet(char *str)
 {
 	int index1 = 0, index2;
+
 	char leet[8] = {'0', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[index1])
 	{
-		for (index2 = 0; index2 <= 8; index2++)
+		for (index2 = 0; index2 <= 7; index2++)
 		{
 			if (str[index1] == leet[index2] ||
 					str[index1] - 32 == leet[index2])
