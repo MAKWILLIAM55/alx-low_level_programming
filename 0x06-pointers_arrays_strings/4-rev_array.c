94be839682dc9098fc749e276bd781499923efa9
MAKWILLIAM55 <makwilliam2020@gmail.com>
2022-10-28 10:28:27 +0000

    Task 4 function reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reverse_arrat - Reverses the content of an array of integers
- * @a: the array of integers to be revered
+ * reverse_array - Reverses the content of an array of integers
+ * @a: the array of integers to be reversed
  * @n: the number of elements in the array
  */
 void reverse_array(int *a, int n)
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
-		tmp = a(n - 1; index];
+		tmp = a[n - 1 - index];
 				a[n - 1 - index] = a[index];
 				a[index] = tmp;
 				}
